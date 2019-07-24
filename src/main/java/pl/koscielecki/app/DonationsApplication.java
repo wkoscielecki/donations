ffd54a3664d4ac718374e7dfc41565ef9272d56a
wkoscielecki <wojciech11@poczta.onet.pl>
2019-07-24 11:52:38 +0200

    first commit

---
+++ src/main/java/pl/koscielecki/app/DonationsApplication.java
@@ -0,0 +1,13 @@
+package pl.koscielecki.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonationsApplication.class, args);
+    }
+
+}
