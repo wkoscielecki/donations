3519f378c3579118c14aa033ab1a13e64a55b71b
wkoscielecki <wojciech11@poczta.onet.pl>
2019-07-24 13:32:09 +0200

    config

---
+++ src/main/java/pl/koscielecki/app/Security/SecurityConfig.java
@@ -0,0 +1,14 @@
+package pl.koscielecki.app.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
