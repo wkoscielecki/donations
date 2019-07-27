96a8d5000a2283f1732fcde1c6bb5267ae0f0b13
wkoscielecki <wojciech11@poczta.onet.pl>
2019-07-27 12:32:28 +0200

    view and images

--- src/main/java/pl/koscielecki/app/Service/UserService.java
+++ src/main/java/pl/koscielecki/app/Service/UserService.java
@@ -1,5 +1,7 @@
 package pl.koscielecki.app.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.koscielecki.app.Repository.UserRepository;
 import pl.koscielecki.app.model.User;
@@ -10,11 +12,14 @@ import java.util.List;
 public class UserService {
 
     private UserRepository userRepository;
+//    @Autowired
+//    private BCryptPasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
     public User saveUser(User user){
+
         userRepository.save(user);
         return user;
     }
